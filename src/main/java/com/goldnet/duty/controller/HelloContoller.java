c64ad148b14eda7cd9ad52d493c1d718af3dd170
Max.Huang <Max.Huang@goldnet.com>
2022-03-16 18:19:03 +0800

    thymeleaf配置ok

--- src/main/java/com/goldnet/duty/controller/HelloContoller.java
+++ src/main/java/com/goldnet/duty/controller/HelloContoller.java
@@ -5,8 +5,10 @@ import com.goldnet.duty.entity.GeStation;
 import com.goldnet.duty.mapper.GeStationMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -21,5 +23,13 @@ public class HelloContoller {
     @ResponseBody
     public List getStation(){
         return geStationMapper.selectList(null);
+    }
+
+    @GetMapping("/hello1")
+    public String getStation(Model model){
+        List<GeStation> geStations = geStationMapper.selectList(null);
+        model.addAttribute("gestations",geStations);
+
+        return "hello";
     }
 }
